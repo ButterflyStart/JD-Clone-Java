4b9919c9f3a861a3a33c2610df5a18855972e3a1
q8142 <test@test.com>
2025-03-18 17:57:49 +0800

    初始项目

---
+++ src/main/java/com/jd/jdmall/JdCloneJavaV23Application.java
@@ -0,0 +1,13 @@
+package com.jd.jdmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdCloneJavaV23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdCloneJavaV23Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/jd/jdmall/JdCloneJavaV23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jd.jdmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdCloneJavaV23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
